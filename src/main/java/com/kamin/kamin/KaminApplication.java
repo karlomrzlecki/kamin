d63999091d2cc2a400ab3ffc54b2c9c39a42a166
Karlo Mrzlečki <km51049@fer.hr>
2022-05-29 19:37:18 +0200

    create spring project

---
+++ src/main/java/com/kamin/kamin/KaminApplication.java
@@ -0,0 +1,13 @@
+package com.kamin.kamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamin/kamin/KaminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamin.kamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
